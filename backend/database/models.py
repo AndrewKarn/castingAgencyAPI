724b5b151f74eab32f7bc0f9ba74692b4482b4f7
drew.karn <andrew.s.karn@gmail.com>
2020-04-15 21:06:42 -0700

    use env var for db

--- backend/database/models.py
+++ backend/database/models.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "castingagency"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+data_base_path = os.getenv('DATABASE_URL')
 
 db = SQLAlchemy()
 
