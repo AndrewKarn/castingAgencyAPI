af12884f4d2de1a2150bada9aa0726bc4770054b
drew.karn <andrew.s.karn@gmail.com>
2020-04-15 17:14:53 -0700

    Finshed app

---
+++ backend/src/app.py
@@ -0,0 +1,239 @@
+import os
+from flask import Flask, request, jsonify, abort
+from sqlalchemy import exc
+import json
+from flask_cors import CORS
+from jose import jwt
+from urllib.request import urlopen
+from auth.auth import requires_auth, AuthError
+from database.models import setup_db, Movie, Actor
+
+app = Flask(__name__)
+setup_db(app)
+CORS(app)
+
+
+############### ROUTES ###############
+# Actors
+#########
+@app.route('/actors')
+@requires_auth('get:actors')
+def get_actors(jwt):
+    return jsonify({
+        'actors': [actor.format() for actor in Actor.query.all()],
+        'success': True,
+    })
+
+
+@app.route('/actors/<actor_id>', methods=['DELETE'])
+@requires_auth('delete:actor')
+def delete_actor(jwt, actor_id):
+    actor_to_delete = Actor.query.get(actor_id)
+    if actor_to_delete:
+        actor_to_delete.delete()
+        return jsonify({
+            'success': True,
+            'deleted': actor_id
+        })
+    else:
+        abort(404)
+
+
+@app.route('/actors', methods=['POST'])
+@requires_auth('post:actor')
+def add_actor(jwt):
+    error = False
+    data = request.get_json()
+    name = data.get('name', '')
+    age = data.get('age', '')
+    gender = data.get('gender', '')
+    exists = Actor.query.filter_by(
+        name=name, age=age, gender=gender).one_or_none()
+    actor_to_add = Actor(
+        name=name,
+        age=age,
+        gender=gender
+    )
+    if not exists and gender and age and name:
+        try:
+            actor_to_add.insert()
+        except:
+            error = True
+        finally:
+            if not error:
+                return jsonify({
+                    'success': True,
+                    'message': f'Actor: {name} was added'
+                })
+            else:
+                actor_to_add.pull_back()
+                return jsonify({
+                    'success': False,
+                    'message': f'Actor: {name} was not added'
+                })
+    else:
+        abort(400)
+        return jsonify({
+            'success': False,
+            'message': 'An actor with those attributes exists already or you are missing a field',
+        })
+
+
+@app.route('/actors/<actor_id>', methods=['PATCH'])
+@requires_auth('patch:actors')
+def update_actor(jwt, actor_id):
+    request_data = request.get_json()
+    new_name = request_data.get('name', False)
+    new_age = request_data.get('age', False)
+    new_gender = request_data.get('gender', False)
+    actor_to_edit = Actor.query.get(actor_id)
+    if actor_to_edit and new_name and new_gender and new_age:
+        actor_to_edit.name = new_name
+        actor_to_edit.age = new_age
+        actor_to_edit.gender = new_gender
+        actor_to_edit.update()
+        return jsonify({
+            'success': True,
+            'message': 'Actor updated'
+        })
+    else:
+        return jsonify({
+            'success': False,
+            'message': 'Unable to update actor'
+        })
+
+
+############### ROUTES ###############
+# Movies
+#####################################
+
+@app.route('/movies')
+@requires_auth('get:movies')
+def get_movies(jwt):
+    return jsonify({
+        'movies': [movie.format() for movie in Movie.query.all()],
+        'success': True,
+    })
+
+
+@app.route('/movies/<movie_id>', methods=['DELETE'])
+@requires_auth('delete:movies')
+def delete_movie(jwt, movie_id):
+    movie_to_delete = Movie.query.get(movie_id)
+    if movie_to_delete:
+        movie_to_delete.delete()
+        return jsonify({
+            'success': True,
+            'deleted': movie_id
+        })
+    else:
+        abort(404)
+
+
+@app.route('/movies', methods=['POST'])
+@requires_auth('post:movies')
+def add_movie(jwt):
+    error = False
+    data = request.get_json()
+    title = data.get('title', False)
+    release_date = data.get('release_date', False)
+    exists = Movie.query.filter_by(
+        title=title, release_date=release_date).one_or_none()
+    movie_to_add = Movie(
+        title=title,
+        release_date=release_date
+    )
+    if not exists and title and release_date:
+        try:
+            movie_to_add.insert()
+        except:
+            error = True
+        finally:
+            if not error:
+                return jsonify({
+                    'success': True,
+                    'message': f'Movie: {title} was added'
+                })
+            else:
+                movie_to_add.pull_back()
+                return jsonify({
+                    'success': False,
+                    'message': f'Movie: {title} was not added'
+                })
+    else:
+        abort(409)
+        return jsonify({
+            'success': False,
+            'message': 'A movie with those attributes exists already',
+        })
+
+
+@app.route('/movies/<movie_id>', methods=['PATCH'])
+@requires_auth('patch:movies')
+def update_movie(jwt, movie_id):
+    request_data = request.get_json()
+    new_title = request_data.get('title', '')
+    new_release_date = request_data.get('release_date', '')
+    movie_to_edit = Movie.query.get(movie_id)
+    if movie_to_edit and new_title != '' and new_release_date:
+        movie_to_edit.title = new_title
+        movie_to_edit.release_date = new_release_date
+        movie_to_edit.update()
+        return jsonify({
+            'success': True,
+            'message': 'Movie updated'
+        })
+    else:
+        abort(400)
+        return jsonify({
+            'success': False,
+            'message': 'Unable to update movie'
+        })
+
+
+############### ROUTES ###############
+# Error Handlers
+#####################################
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+
+@app.errorhandler(404)
+def resourceNotFound(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Resource not found."
+    }), 404
+
+
+@app.errorhandler(AuthError)
+def handleAuthenticationerror(error):
+    return jsonify({
+        'success': False,
+        'error': AuthError
+    })
+
+
+@app.errorhandler(404)
+def resourceNotFound(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Resource not found."
+    }), 404
+
+
+@app.errorhandler(AuthError)
+def handleAuthenticationerror(error):
+    return jsonify({
+        'success': False,
+        'error': AuthError
+    })
---
+++ backend/src/tests/test_app.py
@@ -0,0 +1,249 @@
+import os
+import unittest
+import json
+from database.models import setup_db, Movie, Actor, SQLAlchemy
+from app import app
+
+baseauth_token = os.getenv('TOKEN1')
+second_level_auth_token = os.getenv('TOKEN2')
+maxium_auth = os.getenv('TOKEN3')
+
+class TriviaTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app
+        self.client = app.test_client
+        self.database_name = "casting_test"
+        self.database_path = "postgres://{}/{}".format(
+            'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+        self.app.config['DEBUG'] = False
+        self.headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {baseauth_token}' }
+        self.actor_data = json.dumps({
+            'name': 'Dana Smith',
+            'age': 25,
+            'gender': 'Female'
+        })
+        self.bad_actor_data = json.dumps({
+            'name': '',
+            'age': 251,
+            'gender': 'Female'
+        })
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.session.commit()
+            self.db.drop_all()
+            self.db.session.commit()
+            self.db.create_all()
+            self.db.session.query(Actor).delete()
+            self.db.session.query(Movie).delete()
+            self.db.session.commit()
+##########################
+# Get Tests
+##########################
+    def test_get_actors(self):
+        res = self.client().get('/actors', headers=self.headers)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data.get('success', False), True)
+
+    def test_bad_get_actors(self):
+        self.headers['Authorization'] = 'aaaa'
+        res = self.client().get('/actors', headers=self.headers)
+        self.assertEqual(res.status_code, 401)
+
+    def test_get_movies(self):
+        res = self.client().get('/movies', headers=self.headers)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data.get('success', False))
+
+    def test_bad_get_movies(self):
+        self.headers['Authorization'] = 'aaaa'
+        res = self.client().get('/movies', headers=self.headers)
+        self.assertEqual(res.status_code, 401)
+
+
+
+##########################
+# POST Tests
+##########################
+    def test_post_actors_with_base_permission(self):
+        res = self.client().post('/actors', headers=self.headers, data=self.actor_data)
+        self.assertEqual(res.status_code, 401)
+
+    def test_post_actor_as_director(self):
+        self.headers['Authorization'] = f'Bearer {second_level_auth_token}'
+        res = self.client().post('/actors', headers=self.headers, data=self.actor_data)
+        self.assertEqual(res.status_code, 200)
+        exists = Actor.query.filter_by(name='Dana Smith', age=25, gender='Female').one_or_none()
+        #confirm db save
+        self.assertIsNotNone(exists)
+        Actor.query.filter_by(name='Dana Smith', age=25, gender='Female').one().delete()
+
+    def test_post_actor_as_executive(self):
+        self.headers['Authorization'] = f'Bearer {maxium_auth}'
+        res = self.client().post('/actors', headers=self.headers, data=self.actor_data)
+        self.assertEqual(res.status_code, 200)
+        exists = Actor.query.filter_by(name='Dana Smith', age=25, gender='Female').one_or_none()
+        #confirm db save
+        self.assertIsNotNone(exists)
+        Actor.query.filter_by(name='Dana Smith', age=25, gender='Female').one().delete()
+
+    def test_post_with_bad_data(self):
+        self.headers['Authorization'] = f'Bearer {second_level_auth_token}'
+        res = self.client().post('/actors', headers=self.headers, data=self.bad_actor_data)
+        self.assertEqual(res.status_code, 400)
+        exists = Actor.query.filter_by(name='', age=251, gender='Female').one_or_none()
+        #confirm db  did not save
+        self.assertIsNone(exists)
+
+    def test_post_movies_with_base_permission(self):
+        res = self.client().post('/movies', headers=self.headers, data=self.actor_data)
+        self.assertEqual(res.status_code, 401) 
+
+    def test_post_movie_as_director(self):
+        self.headers['Authorization'] = f'Bearer {second_level_auth_token}'
+        res = self.client().post('/movies', headers=self.headers, data=self.actor_data)
+        self.assertEqual(res.status_code, 401)
+
+    def test_post_movie_as_executive(self):
+        self.headers['Authorization'] = f'Bearer {maxium_auth}'
+        res = self.client().post('/movies', headers=self.headers, data=json.dumps({'title': 'gr8 movie', 'release_date': '2020-03-15'}))
+        self.assertEqual(res.status_code, 200)
+        exists = Movie.query.filter_by(title='gr8 movie', release_date='2020-03-15').one_or_none()
+        #confirm db save
+        self.assertIsNotNone(exists)
+        Movie.query.filter_by(title='gr8 movie', release_date='2020-03-15').one().delete()
+##########################################
+# Patch
+#     Tests  
+##########################################
+
+    def test_patch_as_executive(self):
+        self.headers['Authorization'] = f'Bearer {maxium_auth}'
+        Movie('example', '2020-04-15').insert()
+        movie = Movie.query.first().format()
+        self.assertEqual(movie.get('title', ''), 'example')
+        movie_id = movie.get('id', '')
+        updates = json.dumps({
+            'title': 'new_title',
+            'release_date': '2020-04-20'
+        })
+        res = self.client().patch(f'/movies/{movie_id}', data=updates, headers=self.headers)
+        # Need the DB to sign off on this as well
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual( Movie.query.get(movie_id).format().get('title', ''), 'new_title' )
+
+    def test_patch_as_director(self):
+        self.headers['Authorization'] = f'Bearer {second_level_auth_token}'
+        Movie('directors movie', '2020-04-15').insert()
+        movie = Movie.query.first().format()
+        self.assertEqual(movie.get('title', ''), 'directors movie')
+        movie_id = movie.get('id', '')
+        updates = json.dumps({
+            'title': 'new_title_for_director',
+            'release_date': '2020-04-20'
+        })
+        res = self.client().patch(f'/movies/{movie_id}', data=updates, headers=self.headers)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual( Movie.query.get(movie_id).format().get('title', ''), 'new_title_for_director' )
+   
+    def test_patching_movies_with_base_permission(self):
+        updates = json.dumps({
+            'title': 'new_title',
+            'release_date': '2020-04-20'
+        })
+        res = self.client().patch('/movies/1', data=updates, headers=self.headers)
+
+        self.assertEqual(res.status_code, 401)
+
+    def test_patching_movies_with_bad_data(self):
+        self.headers['Authorization'] = f'Bearer {second_level_auth_token}'
+        updates = json.dumps({
+            'tite': '',
+            'release_date': ''
+        })
+        res = self.client().patch('/movies/1', data=updates, headers=self.headers)
+
+        self.assertEqual(res.status_code, 400)
+
+
+
+##########################################
+# Delete
+#        Tests  
+##########################################
+
+    def test_delete_as_executive(self):
+        self.headers['Authorization'] = f'Bearer {maxium_auth}'
+        Movie('example', '2020-04-15').insert()
+        movie = Movie.query.first().format()
+        self.assertEqual(movie.get('title', ''), 'example')
+        movie_id = movie.get('id', '')
+        res = self.client().delete(f'/movies/{movie_id}', headers=self.headers)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data.get('success', False))
+   
+    def test_delete_movies_with_base_permission(self):
+        Movie('example', '2020-04-15').insert()
+        movie = Movie.query.first().format()
+        self.assertEqual(movie.get('title', ''), 'example')
+        movie_id = movie.get('id', '')
+        res = self.client().delete(f'/movies/{movie_id}', headers=self.headers)
+        self.assertEqual(res.status_code, 401)
+
+    def test_delete_movies_as_director(self):
+        self.headers['Authorization'] = f'Bearer {second_level_auth_token}'
+        Movie('example', '2020-04-15').insert()
+        movie = Movie.query.first().format()
+        self.assertEqual(movie.get('title', ''), 'example')
+        movie_id = movie.get('id', '')
+        res = self.client().delete(f'/movies/{movie_id}', headers=self.headers)
+        self.assertEqual(res.status_code, 401)
+
+    def test_delete_movies_with_bad_data(self):
+        self.headers['Authorization'] = f'Bearer {maxium_auth}'
+        res = self.client().delete('/movies/101', headers=self.headers)
+        self.assertEqual(res.status_code, 404)
+
+
+    def test_delete_actor_as_executive(self):
+        self.headers['Authorization'] = f'Bearer {maxium_auth}'
+        Actor('Hannah Carter', 23, 'Female').insert()
+        actress = Actor.query.first().format()
+        actress_id = actress.get('id', '')
+        self.assertEqual(actress.get('name', ''), 'Hannah Carter')
+        res = self.client().delete(f'/actors/{actress_id}', headers=self.headers)
+        exists = Actor.query.filter_by(name='Hannah Carter', age=23, gender='Female').one_or_none()
+        self.assertEqual(res.status_code, 200)
+        self.assertIsNone(exists)
+
+    def test_delete_actor_as_director(self):
+        self.headers['Authorization'] = f'Bearer {second_level_auth_token}'
+        Actor('Hannah Carter', 23, 'Female').insert()
+        actress = Actor.query.first().format()
+        actress_id = actress.get('id', '')
+        self.assertEqual(actress.get('name', ''), 'Hannah Carter')
+        res = self.client().delete(f'/actors/{actress_id}', headers=self.headers)
+        exists = Actor.query.filter_by(name='Hannah Carter', age=23, gender='Female').one_or_none()
+        self.assertEqual(res.status_code, 200)
+        self.assertIsNone(exists)
+    
+    def test_delete_actor_as_assistant(self):
+        Actor('Hannah Carter', 23, 'Female').insert()
+        actress = Actor.query.first().format()
+        actress_id = actress.get('id', '')
+        self.assertEqual(actress.get('name', ''), 'Hannah Carter')
+        res = self.client().delete(f'/actors/{actress_id}', headers=self.headers)
+        exists = Actor.query.filter_by(name='Hannah Carter', age=23, gender='Female').one_or_none()
+        self.assertEqual(res.status_code, 401)
+        self.assertIsNotNone(exists)
+
+if __name__ == "__main__":
+    unittest.main()
